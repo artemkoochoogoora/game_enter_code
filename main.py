eb1372c2012505e95df013b8f1fdacf01886af63
Alexander Kuchugura <kuchugura@MacBook-Pro-Alexander.local>
2022-01-09 02:06:41 +0300

    Всё точно дописал

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import random
+import smtplib
 print('********  Игра скажи число! ********')
 code = random.randint(10000,100000)
+coders = input("Напиши свою почту:")
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+smtpObj.starttls()
+smtpObj.login('botgameentercode@gmail.com','testbot123')
+message_text = "your code is " + str(code)
+smtpObj.sendmail("botgameentercode@gmail.com",coders,message_text)
 cookies = input("Введите код из почты:")
-if code == cookies :
-    print("Совпал!")
+if str(code) == cookies :
+    print("Ого! А ты крутой =)!")
 else:
     print("неверный код! =(")
